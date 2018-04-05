aac8ab531d82f90c6508e54904b0ce129a8bfc49
Renan Sies <renansies@gmail.com>
2018-04-05 10:09:15 -0300

    Push Notifications

---
+++ static/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+let config = {
+    messagingSenderId: "593761485732"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    return self.registration.showNotification({}, {});
+})
